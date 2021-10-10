c5429078d06ad3e57bc239eaade9872eb157ac82
Susana Villagrana <savi@ucdavis.edu>
2021-10-10 02:26:42 -0700

    Bubble Chart added

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,6 +1,6 @@
 
+// Display the default plot
 function init() {
-
     // Fetch the JSON data and console log it
     const belly_button = "./samples.json"
 
@@ -12,41 +12,8 @@ function init() {
 
         ///////////////////////////////////////////
 
-
-        ///////////// Drop Down Menu /////////////
-
-        let dataNames = data.names;
-        var dropDownMenu = d3.select("#selDataset");
-  
-        dataNames.forEach(function (name) {
-            dropDownMenu.append("option").text(name).property("value", name);
-        });
-
-
         // Selection from Drop Down Menu - Not completed to take in value
         let selectedID = "940";
-
-        //////////// Demographic Info /////////////
-
-        document.getElementById("sample-metadata").text = demoID;
-
-        var item1 = data.metadata
-        var demo = item1.filter((val) => val.id == selectedID)
-
-        for (var key in demo) {
-            if (demo.hasOwnProperty(key)) {
-            var demoID = demo[key].id;
-            var demoEthn = demo[key].ethnicity
-            var demoGen = demo[key].gender
-            var demoAge = demo[key].age
-            var demoLoc = demo[key].location
-            var demoBB = demo[key].bbtype
-            var demoWfreq = demo[key].wfreq
-            }
-            };
-
-
-        console.log("Test: ", demoID)
 
         ////////// Horizontal Bar Chart //////////
 
@@ -58,11 +25,6 @@ function init() {
         // Sort the data by sample values -  descending
         let sorted= item.sort((a, b) => b.sample_values - a.sample_values);
 
-        // datapull(selectedID);
-
-        // function datapull(selectedID)   {
-        //     var subject = sorted.filter((val) => val.id == selectedID);
-        // };
 
         // Filtering Sorted data to keep id equal to drop down selection
         var subject = sorted.filter((val) => val.id == selectedID);
@@ -87,7 +49,7 @@ function init() {
 
         // Logging to console to check values
         // console.log("Sorted: ", sorted);
-        console.log("Subject: ", subject);
+        // console.log("Subject: ", subject);
         // console.log("Sliced Data: ", samp, ids, labels);
         // console.log("Reversed Data: ", reversedSamp, reversedIds, reversedLabels);
 
@@ -110,47 +72,28 @@ function init() {
         // console.log("sample_values: ", item.otu_ids);
         // console.log("otu_ids: ", item.sample_values);
         // console.log("otu_labels: ", item.otu_labels);
-
-        ////////// Bubble Chart //////////
-        
-        for (var key1 in subject) {
-            if (subject.hasOwnProperty(key1)) {
-            var samp2 = subject[key1].sample_values;
-            var ids2 = subject[key1].otu_ids;
-            var labels2 = subject[key1].otu_labels;
-            }
-            };
-
-
-        // Reverse the array to accommodate Plotly's defaults
-        var reversedSamp2 = samp2.reverse();
-        var reversedIds2 = ids2.reverse();
-        var reversedLabels2 = labels2.reverse();
-
-
-        var trace2 = {
-            x: reversedIds2,
-            y: reversedSamp2,
-            text: reversedLabels2,
-            mode: 'markers',
-            marker: {
-              color: reversedIds2,
-              size: reversedSamp2
-            }
-          };
-          
-          let traceData2 = [trace2];
-          
-        var layout ={
-            xaxis: {
-                title: 'OTU ID'}
-        };
-          
-          Plotly.newPlot('bubble', traceData2, layout);
+    
   });
+  }
   
-}
+  // On change to the DOM, call getData()
+  d3.selectAll("#selDataset").on("change", getData);
+  
+  // Function called by DOM changes
+  function getData() {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
 
-init();
+    var selectedID = dataset
 
-
+    // Call function to update the chart
+    return selectedID;
+  }
+  
+  // Update the restyled plot's values
+  function updatePlotly(newdata) {
+    Plotly.restyle("bar", "values", [newdata]);
+  }
+  
+  init();
